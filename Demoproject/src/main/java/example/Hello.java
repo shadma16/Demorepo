999ad445fd1b756158e69a5151cd3a14dcb53779
Shadma Subair CP <145542686+shadma16@users.noreply.github.com>
2025-04-08 14:48:43 +0530

    Update Hello.java

--- Demoproject/src/main/java/example/Hello.java
+++ Demoproject/src/main/java/example/Hello.java
@@ -3,7 +3,7 @@ package example;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");// TODO Auto-generated method stub
+		System.out.println("Hello 123");// TODO Auto-generated method stub
 
 	}
 
